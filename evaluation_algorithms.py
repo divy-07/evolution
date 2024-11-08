147b8c2dafc68bc0d209c12be928a82f85428706
Divy Patel <82171007+divy-07@users.noreply.github.com>
2024-11-08 00:17:36 -0800

    configure and test pre-commit.ci (#1)
    * auto format yaml, toml ,and json
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * autoupdate flake8 version and fix flake8 errors
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- evaluation_algorithms.py
+++ evaluation_algorithms.py
@@ -1,6 +1,7 @@
 """
 The functions in this file convert a list of parameters into a single value in [0,1].
-These are used to determine the creature's evaluation which can then be used to determine the creature's next move, etc using other algorithms.
+These are used to determine the creature's evaluation which can then be used to
+determine the creature's next move, etc using other algorithms.
 """
 
 import math
